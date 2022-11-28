3fe10a4ebc0ab249b48736b6b0ff3b9341ada67e
code-syl <ejjvandelaar@gmail.com>
2022-11-28 11:22:29 +0100

    Added logging to ping controllers

--- src/main/java/com/tick/settingsapi/controllers/ping/PingController.java
+++ src/main/java/com/tick/settingsapi/controllers/ping/PingController.java
@@ -1,15 +1,19 @@
 package com.tick.settingsapi.controllers.ping;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
-@RequestMapping("api/v1/settings/ping")
+@Slf4j
 public class PingController {
 
     @GetMapping
     public String Ping() {
+        log.info(LocalDateTime.now() + "\tInsecure ping request");
         return "Pong!";
     }
 
