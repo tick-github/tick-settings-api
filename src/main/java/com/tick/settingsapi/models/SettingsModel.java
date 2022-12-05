34e7d289358164474dd0c3e7a9855548956d6f55
code-syl <ejjvandelaar@gmail.com>
2022-12-05 15:07:43 +0100

    Added swagger docs

--- src/main/java/com/tick/settingsapi/models/SettingsModel.java
+++ src/main/java/com/tick/settingsapi/models/SettingsModel.java
@@ -1,13 +1,13 @@
 package com.tick.settingsapi.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Builder
 @Getter
 public class SettingsModel {
